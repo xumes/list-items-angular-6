48b049def37c55a837547b1393e8f00ddb689959
Reginaldo Santos <reginaldosantos.br@gmail.com>
2018-09-06 23:31:02 -0300

    counting works

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,13 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  itemCount: number = 4;
+  itemCount: number;
   btnText: string = 'Add and item';
   goalText: string = 'My first life goal';
+  goals = [];
+
+
 
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
   }
 
 }
