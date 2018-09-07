3de55806a2e5cd56a31b22e87b2491bec3115177
Reginaldo Santos <reginaldosantos.br@gmail.com>
2018-09-06 23:52:03 -0300

    animating the remove item

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,7 +17,16 @@ import { trigger, style, transition, animate, keyframes, query, stagger } from '
             style({ opacity: .5, transform: 'translateY(35px)', offset: .3 }),
             style({ opacity: 1, transform: 'translateY(0)', offset: 1 }),
           ]))
+        ]), { optional: true }),
+
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({ opacity: 1, transform: 'translateY(0)', offset: 0 }),
+            style({ opacity: .5, transform: 'translateY(35px)', offset: .3 }),
+            style({ opacity: 0, transform: 'translateY(-75%)', offset: 1 }),
+          ]))
         ]), { optional: true })
+
       ])
     ])
   ]
