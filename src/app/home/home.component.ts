7d61ef997afa53d8d6fb5df83c198b35c6b14839
Reginaldo Santos <reginaldosantos.br@gmail.com>
2018-09-06 23:26:24 -0300

    two way data bind (ngModel)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  itemCount: number = 4;
+  btnText: string = 'Add and item';
+  goalText: string = 'My first life goal';
+
   constructor() { }
 
   ngOnInit() {
